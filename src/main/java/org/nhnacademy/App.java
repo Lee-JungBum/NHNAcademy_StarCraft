4f6d50c99ee09eaf87ccb38715fd11ed753617e6
LeeJungBum <jungbum05114@gmail.com>
2023-10-03 06:06:39 +0900

    feat: 플레이어 종족 선택

--- src/main/java/org/nhnacademy/App.java
+++ src/main/java/org/nhnacademy/App.java
@@ -1,13 +1,34 @@
 package org.nhnacademy;
 
+import java.util.Scanner;
+import org.nhnacademy.races.Races;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String races;
+        do{
+            logger.info("플레이어의 종족을 선택해주세요. ");
+            races=scanner.next();
+        }while(!selectRaces(races));
+
+    }
+    private static boolean selectRaces(String races){
+        try {
+            Races.valueOf(races);
+            return true;
+        } catch (IllegalArgumentException e) {
+            logger.error("정의되지 않은 종족을 선택하였습니다. 다시입력해주세요.");
+            return false;
+        }
     }
 }
