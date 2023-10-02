ae5e3c543278ada1643bef69a07365b81f1dbc89
LeeJungBum <jungbum05114@gmail.com>
2023-10-03 05:54:50 +0900

    feat: Races정의

---
+++ src/main/java/org/nhnacademy/races/Races.java
@@ -0,0 +1,12 @@
+package org.nhnacademy.races;
+
+public enum Races {
+    ZERG("ZERG"),
+    PROTOS("PROTOS"),
+    TERRAN("TERRAN");
+    String races;
+    Races(String races)
+    {
+        this.races=races;
+    }
+}
