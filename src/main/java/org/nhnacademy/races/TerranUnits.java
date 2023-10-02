6c437a5cd25f07a32743c9696b2151c890ecf0e6
LeeJungBum <jungbum05114@gmail.com>
2023-10-03 05:45:51 +0900

    feat: TerranUnits Enum타입 정의

---
+++ src/main/java/org/nhnacademy/races/TerranUnits.java
@@ -0,0 +1,25 @@
+package org.nhnacademy.races;
+
+public enum TerranUnits {
+    Marine(3,10),
+    Tank(7,15),
+    Goliath(5,15),
+    Wraith(3,10),
+    Valkyrie(4,12);
+    private final int def;
+    private final int atk;
+
+    TerranUnits(int def, int atk)
+    {
+        this.def=def;
+        this.atk=atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+}
